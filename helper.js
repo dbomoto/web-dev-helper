81017b4885c75638d29eb839d20f76a3d583f065
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-04-15 23:44:14 +0800

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,11 @@
+/*eslint-env browser*/
+
+// TABLE OF CONTENTS
+// 
+// Instructions: Search sections using the following legend.
+// 
+// Section1 - INITIALIZATIONS
+// Section2 - INPUT COLLECTION
+// Section3 - REFERENCE OBJECTS 
+// Section4 - CSS MODEL FUNCTIONS
+// Section5 - HTML DOM FUNCTIONS
