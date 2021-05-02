8d68f562e0c74bc646ecd1bb33f605bceca442cb
Daniel Benedict L. Omoto <78993631+dbomoto@users.noreply.github.com>
2021-05-02 22:59:18 +0800

    Update server.js
    Update on code.

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log(`Listening on port ${port}`);
+})
